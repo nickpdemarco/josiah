d8e721358076fca18530a71ee7ba590c4d087e52
Nick DeMarco <nicholas_demarco@brown.edu>
2017-04-29 17:42:00 -0400

    [ND] minor typo in user friendly

--- server/src/main/java/edu/brown/cs/ndemarco/josiah/UserFriendly.java
+++ server/src/main/java/edu/brown/cs/ndemarco/josiah/UserFriendly.java
@@ -2,5 +2,5 @@ package edu.brown.cs.ndemarco.josiah;
 
 public class UserFriendly {
 	public static String OTHER_SERVICE_FAILED = "It seems the service I need to handle this request is down. I'm sorry!";
-	public static String EMPTY_DINING_RESPONSE = "It seems Brown Dining hasn't report what's on the menu for that time.";
+	public static String EMPTY_DINING_RESPONSE = "It seems Brown Dining hasn't reported what's on the menu for that time.";
 }
